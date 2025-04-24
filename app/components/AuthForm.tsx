ea28c4e4603632fc1d4594e67ac626614566e1ff
Ishita Majumdar <majumdar.ishita2003@gmail.com>
2025-04-25 00:38:20 +0530

    fix: correct JSX structure in AuthForm component

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -38,9 +38,16 @@ const authform = () => {
     console.log(values)
   }
   return (
-    <div>
+    <div className="card-border lg:min-w-[566px] ">
+        <div className="flex flex-col gap-6 card py-14 px-10">
+            <div className="flex flex-row gap-2 justify-center">
+                <Image src="/logo.svg" alt="logo" height={32} width={38} />
+                <h2 className="text-primary-100">PrepWise</h2>
+            </div>
+            <h3>Practice job interview with AI</h3>
         <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} 
+      className="w-full space-y-6 mt=4 form">
         <FormField
           control={form.control}
           name="username"
@@ -61,6 +68,8 @@ const authform = () => {
       </form>
     </Form>
     </div>
+    </div>
+
   )
 }
 
