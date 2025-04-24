94a7074366bb1ad5681d228672d97b2cbef9177b
Ishita Majumdar <majumdar.ishita2003@gmail.com>
2025-04-24 20:19:49 +0530

    Add SVG icon for window and utility functions for tech logos and interview covers
    - Introduced a new SVG file for the window icon.
    - Implemented utility functions in utils.ts for:
      - Merging class names with Tailwind CSS.
      - Normalizing technology names for logo retrieval.
      - Checking if tech icons exist via HEAD requests.
      - Fetching technology logos based on an array of tech names.
      - Randomly selecting an interview cover from a predefined list.

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import {ReactNode} from 'react'
+
+const RootLayout = ({children}:{children: ReactNode} ) => {
+  return (
+    <div>layout</div>
+  )
+}
+
+export default RootLayout
+
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Mona_Sans } from "next/font/google";
+import { Geist, Geist_Mono, Mona_Sans } from "next/font/google";
 import "./globals.css";
 
 const monaSans = Mona_Sans({
-  variable: "--font-mona-sans",
+  variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
 
 export const metadata: Metadata = {
   title: "PrepWise",
-  description: "An AI-powered platdorm for mock interviews",
+  description: "An AI powered platform for preparing for mock interviews",
 };
 
 export default function RootLayout({
@@ -19,9 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body
-        className={`${monaSans.className} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
