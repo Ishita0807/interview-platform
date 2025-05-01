dab95bd72b2da884b67a225378db4e7656850771
Ishita Majumdar <majumdar.ishita2003@gmail.com>
2025-05-01 11:20:46 +0530

    feat: add Firebase Admin SDK integration and initialize Firestore and Auth services

---
+++ Firebase/Admin.ts
@@ -0,0 +1,31 @@
+import {initializeApp, getApps, cert} from 'firebase-admin/app';
+import { init } from 'next/dist/compiled/webpack/webpack';
+import { getFirestore as adminGetFirestore } from 'firebase-admin/firestore';
+import { getAuth as adminGetAuth } from 'firebase-admin/auth';
+const InitFirebaseAdmin = () => {
+    const apps= getApps();
+
+    if(!apps.length){
+        initializeApp({
+            credential: cert({
+                projectId: process.env.FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            }),
+        });
+    }
+
+return{
+    auth: getAuth(),
+    db: getFirestore()
+    }
+}
+function getFirestore() {
+    return adminGetFirestore();
+}
+
+export const { auth, db } = InitFirebaseAdmin();
+
+function getAuth() {
+    return adminGetAuth();
+}
