fc014503c5569357dc26fdf2b018eddc3b776ec0
Ishita Majumdar <majumdar.ishita2003@gmail.com>
2025-05-01 19:12:27 +0530

    refactor: update import path for InterviewCard component to maintain consistency

--- Firebase/Admin.ts
+++ Firebase/Admin.ts
@@ -49,7 +49,7 @@ const InitFirebaseAdmin = () => {
     console.log({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
+        privateKey: process.env.FIREBASE_PROJECT_KEY,
     });
     return {
         auth: getAuth(),
