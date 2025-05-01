315a9ee8d6c8534b42573c35f9012bb5c6c18494
Ishita Majumdar <majumdar.ishita2003@gmail.com>
2025-05-01 11:32:22 +0530

    feat: implement authentication actions with session management and user handling

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -3,7 +3,7 @@ import { z } from "zod";
 import Link from "next/link";
 import Image from "next/image";
 import { toast } from "sonner";
-import { auth } from "@/firebase/client";
+import { auth } from "@/Firebase/Client";
 import { useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
